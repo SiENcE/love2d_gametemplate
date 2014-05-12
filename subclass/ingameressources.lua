0b686db580249075bd2d0286255d892313f5dc81
Florian <seabeams@gmx.de>
2014-05-12 15:51:01 +0200

    -more ressources to load

--- subclass/ingameressources.lua
+++ subclass/ingameressources.lua
@@ -10,5 +10,14 @@ res.images = {
 				['A_left-hand-climber-hi3'] = { 244, 602, 120, 179, 406, 961 },
 		}
 	},
+	{'left-hand-climber-hi4', 'media/images/left-hand-climber-hi.png' },
+	{'left-hand-climber-hi5', 'media/images/left-hand-climber-hi2.png' },
+	{'left-hand-climber-hi6', 'media/images/left-hand-climber-hi3.png' },
+	{'left-hand-climber-hi7', 'media/images/left-hand-climber-hi.png' },
+	{'left-hand-climber-hi8', 'media/images/left-hand-climber-hi2.png' },
+	{'left-hand-climber-hi9', 'media/images/left-hand-climber-hi3.png' },
+	{'left-hand-climber-hi10', 'media/images/left-hand-climber-hi.png' },
+	{'left-hand-climber-hi11', 'media/images/left-hand-climber-hi2.png' },
+	{'left-hand-climber-hi12', 'media/images/left-hand-climber-hi3.png' },
 }
 return res
