42dd2976d3f8222b29d5577e966ec37f79919d16
Florian <seabeams@gmx.de>
2015-03-12 16:34:43 +0100

    -game template updated for shoebox

--- subclass/ingameressources.lua
+++ subclass/ingameressources.lua
@@ -5,10 +5,10 @@ res.image = {
 	{'left-hand-climber-hi3', 'media/images/left-hand-climber-hi3.png' },
 	{'textureatlas', 'media/images/textureatlas.png',
 		quad = {
-				['A_left-hand-climber-hi'] = { 2, 2, 402, 598, 406, 961 },
-				['A_left-hand-climber-hi2'] = { 2, 602, 240, 357, 406, 961 },
-				['A_left-hand-climber-hi3'] = { 244, 602, 120, 179, 406, 961 },
-		}
+			['A_left-hand-climber-hi']={ 0, 0, 402, 598, 512, 1024, 0, 0, 402, 598 },
+			['A_left-hand-climber-hi2']={ 0, 600, 240, 357, 512, 1024, 0, 0, 240, 357 },
+			['A_left-hand-climber-hi3']={ 242, 600, 120, 179, 512, 1024, 0, 0, 120, 179 }
+		},
 	},
 	{'left-hand-climber-hi4', 'media/images/left-hand-climber-hi.png' },
 	{'left-hand-climber-hi5', 'media/images/left-hand-climber-hi2.png' },
